db3a031fb8596a4680485358175bb7b6bb11f69f
jsamweb <jsam.web@gmail.com>
2021-05-17 23:04:31 +0200

    v2.0 aprendiendo hacer un ciclo y funcniones

---
+++ canvas/form.js
@@ -0,0 +1,9 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
+function dibujoPorClick()
+{
+    console.log(texto);
+    // alert("Epa Epa EEEEEEPAAAAAAAAAA");
+}
