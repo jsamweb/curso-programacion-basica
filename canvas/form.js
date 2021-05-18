6166f785a34f626a1bbb8b174ec03a87ef4bc575
jsamweb <jsam.web@gmail.com>
2021-05-18 22:32:12 +0200

    v1.0 pizarra

--- canvas/form.js
+++
@@ -1,9 +0,0 @@
-var texto = document.getElementById("texto_lineas");
-var boton = document.getElementById("botoncito");
-boton.addEventListener("click", dibujoPorClick);
-
-function dibujoPorClick()
-{
-    console.log(texto);
-    // alert("Epa Epa EEEEEEPAAAAAAAAAA");
-}
