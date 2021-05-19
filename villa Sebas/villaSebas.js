3bd8d87b02235d5ddd779ea29aef0639bd88f41c
jsamweb <jsam.web@gmail.com>
2021-05-19 20:29:14 +0200

    v0.1 no me aparece nada en la pantalla

---
+++ villa Sebas/villaSebas.js
@@ -0,0 +1,12 @@
+var z;
+
+for(var i=10; i<10; i++){
+    document.write(z + ", ");
+    z = aleatorio(1, 15);
+}
+
+function aleatorio(min, maxi){
+    var resultado;
+    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+    return resultado;
+}
