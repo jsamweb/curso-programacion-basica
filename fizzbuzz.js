2a9facbb06c2a392c06b959223ddd25ef463b1a4
jsamweb <jsam.web@gmail.com>
2021-05-21 18:51:17 +0200

    como hacer fizz buzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,26 @@
-alert("me cago en dios");
+var numeros = 100;
+var divisible =false;
+for (var i = 1; i <= 100; i++)
+{
+    if (esDivisible (i,3)){
+        document.write("Fizz");
+        divisible = true;
+    }
+        document.write("Buzz")
+        divisible = true;
+    }
+    if(!esDivisible (i,3) && !esDivisible (i,5)){
+        document.write(i);
+    }
+
+    document.write("<br>");
+    
+}
+function esDivisible(num, divisor){
+    if (num % divisor == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
